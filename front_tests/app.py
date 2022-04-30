b8dfcd78a84072f45ff051ac5b6e82cdc51e0557
Simon Owerien <simon.owerien@live.de>
2022-04-30 16:15:10 +0200

    modified frontend

--- front_tests/app.py
+++ front_tests/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def landing_page():  # put application's code here
     return render_template('index.html')
 
 
